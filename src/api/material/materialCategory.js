0a0243d4dbc634db65260fb1a56b76dd95579ef6
程安宁 <3308869544@qq.com>
2024-12-15 19:01:00 +0800

    feat: 物料分类

--- src/api/material/materialCategory.js
+++ src/api/material/materialCategory.js
@@ -9,4 +9,45 @@ export function listMaterialCategory(query) {
   })
 }
 
+// 查询物料分类详细
+export function getMaterialCategory(id) {
+  return request({
+    url: '/material/materialCategory/' + id,
+    method: 'get'
+  })
+}
 
+// 新增物料分类
+export function addMaterialCategory(data) {
+  return request({
+    url: '/material/materialCategory',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改物料分类
+export function updateMaterialCategory(data) {
+  return request({
+    url: '/material/materialCategory',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除物料分类
+export function delMaterialCategory(id) {
+  return request({
+    url: '/material/materialCategory/' + id,
+    method: 'delete'
+  })
+}
+
+// 导出物料分类
+export function exportMaterialCategory(query) {
+  return request({
+    url: '/material/materialCategory/export',
+    method: 'get',
+    params: query
+  })
+}
--- src/views/material/materialCategory/index.vue
+++ src/views/material/materialCategory/index.vue
@@ -1,22 +1,244 @@
 <template>
+  <div class="app-container">
+    <el-form @submit.native.prevent :model="queryParams" ref="queryForm" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="分类编码" prop="code">
+        <el-input v-model="queryParams.code" placeholder="请输入分类编码" clearable size="small"
+          @keyup.enter.native="handleQuery" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
 
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button type="primary" plain icon="el-icon-plus" size="mini" @click="handleAdd"
+          v-hasPermi="['material:materialCategory:add']">新增</el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="materialCategoryList" row-key="id" default-expand-all
+      :tree-props="{ children: 'children', hasChildren: 'hasChildren' }">
+      <el-table-column label="分类编码" align="center" prop="code" />
+      <el-table-column label="分类名称" align="center" prop="name" />
+      <el-table-column label="说明" align="center" prop="remark" />
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button size="mini" type="text" icon="el-icon-edit" @click="handleAdd(scope.row)"
+            v-hasPermi="['material:materialCategory:edit']">新增</el-button>
+          <el-button size="mini" type="text" icon="el-icon-edit" @click="handleUpdate(scope.row)"
+            v-hasPermi="['material:materialCategory:edit']">修改</el-button>
+          <el-button size="mini" type="text" icon="el-icon-delete" @click="handleDelete(scope.row)"
+            v-hasPermi="['material:materialCategory:remove']">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- 添加或修改物料分类对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="父级分类" prop="parentId">
+          <Treeselect v-model="form.parentId" :options="materialCategoryOptions" :normalizer="normalizer"
+            placeholder="请选择父类ID"></Treeselect>
+        </el-form-item>
+        <el-form-item label="分类编码" prop="code">
+          <el-input v-model="form.code" placeholder="请输入分类编码" />
+        </el-form-item>
+        <el-form-item label="分类名称" prop="name">
+          <el-input v-model="form.name" placeholder="请输入分类名称" />
+        </el-form-item>
+        <el-form-item label="说明" prop="remark">
+          <el-input v-model="form.remark" placeholder="请输入说明" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
+import { listMaterialCategory, getMaterialCategory, delMaterialCategory, addMaterialCategory, updateMaterialCategory, exportMaterialCategory } from "@/api/material/materialCategory";
+import Treeselect from "@riophae/vue-treeselect";
+import "@riophae/vue-treeselect/dist/vue-treeselect.css";
 
 export default {
   name: "MaterialCategory",
-  
+  components: {
+    Treeselect
+  },
   data() {
     return {
-      
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 物料分类表格数据
+      materialCategoryList: [],
+      // 物料分类数
+      materialCategoryOptions: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        code: null,
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+      }
     };
   },
   created() {
-  
+    this.getList();
   },
   methods: {
-    
+    /** 查询物料分类列表 */
+    getList() {
+      this.loading = true;
+      listMaterialCategory(this.queryParams).then(response => {
+        this.materialCategoryList = this.handleTree(response.data, 'id', 'parentId');
+        this.loading = false;
+      });
+    },
+    normalizer(node) {
+      if (node.children && !node.children.length) {
+        delete node.children;
+      }
+
+      return {
+        id: node.id,
+        label: node.name,
+        children: node.children
+      }
+    },
+    getTreeSelect() {
+      listMaterialCategory().then(response => {
+        this.materialCategoryOptions = [];
+        const data = { id: 0, name: '分类', children: [] };
+        data.children = this.handleTree(response.data, "id", "parentId");
+        this.materialCategoryOptions.push(data)
+      })
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        id: null,
+        code: null,
+        name: null,
+        remark: null,
+        parentId: null,
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length !== 1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd(row) {
+      this.reset();
+      if (row != null) {
+        this.form.parentId = row.id;
+      }
+      this.getTreeSelect()
+      this.open = true;
+      this.title = "添加物料分类";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      this.getTreeSelect()
+      if (row != null) {
+        this.form.parentId = row.id;
+      }
+      getMaterialCategory(row.id).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改物料分类";
+      });
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id != null) {
+            updateMaterialCategory(this.form).then(response => {
+              this.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
+          } else {
+            addMaterialCategory(this.form).then(response => {
+              this.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const ids = row.id || this.ids;
+      this.$confirm('是否确认删除物料分类编号为"' + ids + '"的数据项?', "警告", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(function () {
+        return delMaterialCategory(ids);
+      }).then(() => {
+        this.getList();
+        this.msgSuccess("删除成功");
+      })
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      const queryParams = this.queryParams;
+      this.$confirm('是否确认导出所有物料分类数据项?', "警告", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(function () {
+        return exportMaterialCategory(queryParams);
+      }).then(response => {
+        this.download(response.msg);
+      })
+    }
   }
 };
 </script>
